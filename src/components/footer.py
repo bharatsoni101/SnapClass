600fc075d043112c83545eb6d3011ef2b78182bc
Bharat Soni <bharat.soni101@gmail.com>
2026-05-24 19:52:28 +0530

    Added name

--- src/components/footer.py
+++ src/components/footer.py
@@ -8,7 +8,8 @@ def footer_home():
     st.markdown(f""" 
         <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center" >
             <p style="font-weight:bold; color: white; ">Created with ❤️ by </p>
-            <img src='{logo_url}' style='height: 25px;' ></img>
+            <p style="font-weight:bold; color: white; ">BHARAT SONI</p>
+            <!--<img src='{{ url_for('img', filename='/bharatsoni_logo.png') }}' style='height: 25px;' ></img>-->
         </div>
     
     """, unsafe_allow_html=True)
@@ -21,7 +22,8 @@ def footer_dashboard():
     st.markdown(f""" 
         <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center" >
             <p style="font-weight:bold; color: black; ">Created with ❤️ by </p>
-            <img src='{logo_url}' style='height: 25px;' ></img>
+            <p style="font-weight:bold; color: black; ">BHARAT SONI</p>
+            <!--<img src='{{ url_for('img', filename='/bharatsoni_logo.png') }}' style='height: 25px;' ></img>-->
         </div>
     
     """, unsafe_allow_html=True)
